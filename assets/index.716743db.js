0fd889c658cc13548ad6704eb07e3dc9506281b1
klar-sites <klar.sites@twixly.io>
2022-12-28 20:24:30 +0100

    Commit to site!

--- assets/index.716743db.js
+++ assets/index.716743db.js
@@ -1,4 +1,50 @@
-function Dp(e,t){for(var n=0;n<t.length;n++){const r=t[n];if(typeof r!="string"&&!Array.isArray(r)){for(const l in r)if(l!=="default"&&!(l in e)){const o=Object.getOwnPropertyDescriptor(r,l);o&&Object.defineProperty(e,l,o.get?o:{enumerable:!0,get:()=>r[l]})}}}return Object.freeze(Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}))}(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const l of document.querySelectorAll('link[rel="modulepreload"]'))r(l);new MutationObserver(l=>{for(const o of l)if(o.type==="childList")for(const i of o.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&r(i)}).observe(document,{childList:!0,subtree:!0});function n(l){const o={};return l.integrity&&(o.integrity=l.integrity),l.referrerpolicy&&(o.referrerPolicy=l.referrerpolicy),l.crossorigin==="use-credentials"?o.credentials="include":l.crossorigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function r(l){if(l.ep)return;l.ep=!0;const o=n(l);fetch(l.href,o)}})();function Bp(e){return e&&e.__esModule&&Object.prototype.hasOwnProperty.call(e,"default")?e.default:e}var P={exports:{}},B={};/**
+(function () {
+function TemplateHero(props) {
+  const {
+    title,
+    subtitle
+  } = props.block;
+  const {
+    Section,
+    Container,
+    Button,
+    Link
+  } = props.ui;
+  return /*#__PURE__*/React.createElement(Section, {
+    className: "bg-slate-900 text-on-background"
+  }, /*#__PURE__*/React.createElement(Container, null, /*#__PURE__*/React.createElement("div", {
+    className: "text-center text-inverse-on-background mx-auto max-w-3xl px:4 sm:px-16 pb-32 pt-20 sm:pt-40 sm:pb-48"
+  }, /*#__PURE__*/React.createElement("h1", {
+    className: "text-4xl font-bold tracking-tight sm:text-center sm:text-6xl"
+  }, title), /*#__PURE__*/React.createElement("p", {
+    className: "mt-6 text-lg leading-8 sm:text-center"
+  }, subtitle), /*#__PURE__*/React.createElement("div", {
+    className: "mt-8 flex gap-x-4 sm:justify-center"
+  }, /*#__PURE__*/React.createElement(Link, {
+    className: "bg-primary text-on-primary inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 shadow-sm hover:bg-primary-dark",
+    to: "/sida-1"
+  }, "Sida 1"), /*#__PURE__*/React.createElement("a", {
+    href: "#",
+    className: "bg-primary text-on-primary inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 shadow-sm hover:bg-primary-dark"
+  }, "Learn more ", /*#__PURE__*/React.createElement("span", {
+    class: "text-on-primary",
+    "aria-hidden": "true"
+  }, "\u2192")), /*#__PURE__*/React.createElement("a", {
+    href: "#",
+    className: "inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-neutral-200 ring-1 ring-gray-200/10 hover:ring-gray-200/20"
+  }, "Live demo ", /*#__PURE__*/React.createElement("span", {
+    class: "text-neutral-400",
+    "aria-hidden": "true"
+  }, "\u2192"))))));
+}
+const templateComponents = {
+  TemplateHero: TemplateHero
+  // TemplateTeam: TemplateTeam,
+};
+
+window.templateComponents = templateComponents;
+// console.log(window.templateComponents);
+})();function Dp(e,t){for(var n=0;n<t.length;n++){const r=t[n];if(typeof r!="string"&&!Array.isArray(r)){for(const l in r)if(l!=="default"&&!(l in e)){const o=Object.getOwnPropertyDescriptor(r,l);o&&Object.defineProperty(e,l,o.get?o:{enumerable:!0,get:()=>r[l]})}}}return Object.freeze(Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}))}(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const l of document.querySelectorAll('link[rel="modulepreload"]'))r(l);new MutationObserver(l=>{for(const o of l)if(o.type==="childList")for(const i of o.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&r(i)}).observe(document,{childList:!0,subtree:!0});function n(l){const o={};return l.integrity&&(o.integrity=l.integrity),l.referrerpolicy&&(o.referrerPolicy=l.referrerpolicy),l.crossorigin==="use-credentials"?o.credentials="include":l.crossorigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function r(l){if(l.ep)return;l.ep=!0;const o=n(l);fetch(l.href,o)}})();function Bp(e){return e&&e.__esModule&&Object.prototype.hasOwnProperty.call(e,"default")?e.default:e}var P={exports:{}},B={};/**
  * @license React
  * react.production.min.js
  *
